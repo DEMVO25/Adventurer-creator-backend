ef7a3f202c3e0ed66e96fe521032c33b36df513b
DEMVO25 <141271965+DEMVO25@users.noreply.github.com>
2024-10-14 21:11:35 +0300

    expended characters table

--- backend.js
+++ backend.js
@@ -7,7 +7,7 @@ app.use(express.json()); // Add JSON body parsing middleware
 const db = new sqlite3.Database('./DATABASE.db');
 
 db.run('CREATE TABLE IF NOT EXISTS users(username text NOT NULL UNIQUE, id INTEGER PRIMARY KEY AUTOINCREMENT, password text NOT NULL)');
-db.run('CREATE TABLE IF NOT EXISTS characters(name text NOT NULL UNIQUE, username	TEXT NOT NULL, FOREIGN KEY(username) REFERENCES users (username))')
+db.run('CREATE TABLE IF NOT EXISTS characters(name text NOT NULL UNIQUE, adventurername text NOT NULL UNIQUE, classlevel TEXT NOT NULL, background TEXT NOT NULL, race TEXT NOT NULL, alignment TEXT NOT NULL, experience INTEGER, strengthmod TEXT NOT NULL, strengthnumber INTEGER NOT NULL, dexmod TEXT NOT NULL, dexnumber INTEGER NOT NULL, username	TEXT NOT NULL, constitutionmod TEXT NOT NULL, constitutionnumber INTEGER NOT NULL,intelligencemod TEXT NOT NULL, intelligencenumber INTEGER NOT NULL, wisdommod TEXT NOT NULL, wisdomnumber INTEGER NOT NULL, charismamod TEXT NOT NULL, charismanumber INTEGER NOT NULL, inspiration INTEGER, proficiencybonus TEXT NOT NULL, strengthsavingthrow TEXT,dexteritysavingthrow TEXT,constitutionsavingthrow TEXT,intelligencesavingthrow TEXT,wisdomsavingthrow TEXT,charismasavingthrow TEXT,acrobaticscheck TEXT,animalhandling TEXT,arcana TEXT,athletics TEXT,deception TEXT,history TEXT,insight TEXT,intimidation TEXT,investigation TEXT,medicine TEXT,nature TEXT, FOREIGN KEY(username) REFERENCES users (username))')
 
 const port = process.env.Port || 3001;
 
